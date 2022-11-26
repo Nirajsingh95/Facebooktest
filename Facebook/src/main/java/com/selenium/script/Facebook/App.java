9d34a8475c19b90e2fb72f04aac9afbc922390aa
ravi <ravindrakuna226@gmail.com>
2022-11-26 10:41:30 +0530

    committing changes

---
+++ Facebook/src/main/java/com/selenium/script/Facebook/App.java
@@ -0,0 +1,30 @@
+package com.selenium.script.Facebook;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	@Test
+    public static void main( String[] args ) throws InterruptedException
+    {
+       
+	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\RAVINDRA\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.id("email")).sendKeys("ravindra226@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("");
+		driver.findElement(By.name("loginbutton")).click();
+		//System.out.println("Script executed successfully");
+		
+    }
+}
