881f74a57d810ee420d53056bb5987173c196490
Niraj <nirajs712884@gmail.com>
2022-12-01 00:24:26 +0530

    new update

--- Facebook/src/main/java/com/selenium/script/Facebook/App.java
+++ Facebook/src/main/java/com/selenium/script/Facebook/App.java
@@ -17,11 +17,11 @@ public class App
     {
        
 	
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\RAVINDRA\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\Users\niraj\Downloads\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.findElement(By.id("email")).sendKeys("ravindra226@gmail.com");
+		driver.findElement(By.id("email")).sendKeys("nirajs712884@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("");
 		driver.findElement(By.name("loginbutton")).click();
 		//System.out.println("Script executed successfully");
